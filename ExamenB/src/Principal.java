5626cb4f7bce4676cab22c3b4e97d8fe6a7c929d
MAC <porrito@192.168.83.3>
2014-04-06 20:29:28 +0200

    Subiendo el proyecto...

---
+++ ExamenB/src/Principal.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		
+		Scanner leer = new Scanner(System.in);
+		int seleccionar;
+		
+		System.out.println("1. Seleccionar profesor");
+		System.out.println("2. Seleccionar alumno");
+		System.out.println("S. Salir");
+		
+		String opcion = leer.nextLine();
+				
+		switch(opcion){
+		
+			case "1":
+					miguelAngelCamarena();
+				
+				break;
+			case "2":
+				
+				System.out.println("ALUMNOS"); //////// Debajo de esta linea ir escribiendo vuestros nombres para formar el menu correspondiente /////
+				// 1. Pikachu Rodriguez
+				// 2. Son Goku Garcia 
+				// ...
+				seleccionar = leer.nextLine();
+				
+				//////// En este swith cada alumno debe añadir un case diferente. ////////
+				//////// Dentro de él, se encontrará una llamada a una funcion con su nombre y apellidos (igual que hice yo en el caso de arriba) ////////
+				switch(){  
+						
+				
+				}
+				
+				break;
+			
+		}
+
+	}
+	
+	public static void miguelAngelCamarena(){
+		System.out.println("Esta es la funcion de Miguel Angel Camarena");
+	}
+
+}
